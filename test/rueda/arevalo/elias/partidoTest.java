aa02575785e7855be7cc8fa6b62b324a87218947
elias <eruedaarevalo@alumno.unlam.edu.ar>
2021-07-06 19:25:35 -0300

    commit de prueba

---
+++ test/rueda/arevalo/elias/partidoTest.java
@@ -0,0 +1,14 @@
+package rueda.arevalo.elias;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class partidoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
